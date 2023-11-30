e8ff861aafda4b24056ad6bd95d1dfaa6bf077c7
Jitendra Kumar Bijarnia <70063345+jitendrakumar0@users.noreply.github.com>
2023-11-30 17:06:31 +0530

    jk

--- webpack.mix.cjs
+++ webpack.mix.cjs
@@ -1,7 +1,7 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css')
+mix.sass('resources/sass/app.scss', 'public/css')
+   // .js('resources/js/app.js', 'public/js')
    .options({
       processCssUrls: false,
       postCss: [require('tailwindcss')],
