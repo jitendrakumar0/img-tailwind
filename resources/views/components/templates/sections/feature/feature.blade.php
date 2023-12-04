f6b3d3e92a3c662c23ee9b213a8307393aa198c7
shivtej <shivtej.img@gmail.com>
2023-12-04 09:24:43 +0530

    feature

--- resources/views/components/templates/sections/feature/feature.blade.php
+++ resources/views/components/templates/sections/feature/feature.blade.php
@@ -6,17 +6,221 @@
 @endpush
 
 @section('content')
-<div class="container bg-theme1 mx-auto p-10">
-    <div class="flex flex-row w-[100%] gap-3">
-        <div class=" p-5 w-[50%] bg-white/5 backdrop-blur-sm rounded-[20px] border-[1px] border-solid border-[white]/10">
-            <div class="flex items-center">
-                <div>
-                    <div class="w-14 h-14  bg-[white]/10 border-[1px]  border-[white]/10 p-2 rounded-[23px]   flex items-center justify-center">   <img src="{{asset("img/Browser.svg")}}" alt="thanku" class="img-fluid w-100 h-100 z-10 "></div>
+<div class=" bg-theme1 py-10 relative overflow-hidden  after:absolute after:right-0 after:top-0 after:bg-[url('../../../../../../img/hero-dot-bg.png')] after:bg-cover after:bg-center after:bg-no-repeat after:h-[600px] after:w-[600px] after:scale-[1.8]">
+    <div class="container mx-auto z-30 after:absolute after:w-20 after:rounded-full after:h-20 after:right-[50px] after:bottom-[50px] after:bg-gradient-to-br after:from-blue-300 after:to-green-500  before:absolute before:w-20 before:rounded-full before:h-20 before:lg:left-[500px] md:left-[300px] :left-[200px] before:top-[300px] before:bg-gradient-to-br before:from-pink-400 before:to-pink-600 ">
+        <div class="ps-4">
+            <div class="lg:text-6xl md:text-5xl sm:text-4xl text-3xl font-bold text-white">Alchemy of Excellence</div>
+            <div class="text-white font-normal md:text-[24px] sm:text-[20px] text-[18px] mt-1">Catalyst for your brand's transformative journey</div>
+        </div>
+        <div class="flex flex-row g-3 flex-wrap w-full mt-3 relative z-40">
+            <div class="md:w-1/2 w-full lg:p-4 md:p-3 p-2">
+                <div class="md:p-5 p-4  bg-white/5 backdrop-blur-sm rounded-[20px] border-[1px] border-solid border-[white]/10">
+                    <div class="flex items-center">
+                        <div>
+                            <div class="sm:w-14 sm:h-14  w-12 h-12 bg-[white]/10 border-[1px]  border-[white]/10 p-1 rounded-[23px]   flex items-center justify-center">   <img src="{{asset("img/Browser.svg")}}" alt="thanku" class="img-fluid w-100 h-100 z-10 "></div>
+                        </div>
+                        <div class="ms-3">
+                            <div class="text-white font-normal md:text-[20px] text-[18px]">Digital Marketing Services</div>
+                            <div class="text-white md:text-[15px] text-sm font-light mt-3  text-ellipsis line-clamp-3 overflow-hidden ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus, illo nem nihil vitae omnis alias illo exercitationem deserunt natus.</div>
+                        </div>
+                    </div>
                 </div>
-                <div class="ms-3">
-                    <div class="text-white font-semibold text-[20px]">Digital Marketing Services</div>
-                    <div class="text-white text-[16px] font-normal mt-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus, illo nem nihil vitae omnis alias illo exercitationem deserunt natus.</div>
+            </div>
+            <div class="md:w-1/2 w-full lg:p-4 md:p-3 p-2">
+                <div class="md:p-5 p-4  bg-white/5 backdrop-blur-sm rounded-[20px] border-[1px] border-solid border-[white]/10">
+                    <div class="flex items-center">
+                        <div>
+                            <div class="sm:w-14 sm:h-14  w-12 h-12 bg-[white]/10 border-[1px]  border-[white]/10 p-1 rounded-[23px]   flex items-center justify-center">   <img src="{{asset("img/Browser.svg")}}" alt="thanku" class="img-fluid w-100 h-100 z-10 "></div>
+                        </div>
+                        <div class="ms-3">
+                            <div class="text-white font-normal md:text-[20px] text-[18px]">Digital Marketing Services</div>
+                            <div class="text-white md:text-[15px] text-sm font-light mt-3  text-ellipsis line-clamp-3 overflow-hidden ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus, illo nem nihil vitae omnis alias illo exercitationem deserunt natus.</div>
+                        </div>
+                    </div>
                 </div>
+            </div>
+            <div class="md:w-1/2 w-full lg:p-4 md:p-3 p-2">
+                <div class="md:p-5 p-4  bg-white/5 backdrop-blur-sm rounded-[20px] border-[1px] border-solid border-[white]/10">
+                    <div class="flex items-center">
+                        <div>
+                            <div class="sm:w-14 sm:h-14  w-12 h-12 bg-[white]/10 border-[1px]  border-[white]/10 p-1 rounded-[23px]   flex items-center justify-center">   <img src="{{asset("img/Browser.svg")}}" alt="thanku" class="img-fluid w-100 h-100 z-10 "></div>
+                        </div>
+                        <div class="ms-3">
+                            <div class="text-white font-normal md:text-[20px] text-[18px]">Digital Marketing Services</div>
+                            <div class="text-white md:text-[15px] text-sm font-light mt-3  text-ellipsis line-clamp-3 overflow-hidden ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus, illo nem nihil vitae omnis alias illo exercitationem deserunt natus.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="md:w-1/2 w-full lg:p-4 md:p-3 p-2">
+                <div class="md:p-5 p-4  bg-white/5 backdrop-blur-sm rounded-[20px] border-[1px] border-solid border-[white]/10">
+                    <div class="flex items-center">
+                        <div>
+                            <div class="sm:w-14 sm:h-14  w-12 h-12 bg-[white]/10 border-[1px]  border-[white]/10 p-1 rounded-[23px]   flex items-center justify-center">   <img src="{{asset("img/Browser.svg")}}" alt="thanku" class="img-fluid w-100 h-100 z-10 "></div>
+                        </div>
+                        <div class="ms-3">
+                            <div class="text-white font-normal md:text-[20px] text-[18px]">Digital Marketing Services</div>
+                            <div class="text-white md:text-[15px] text-sm font-light mt-3  text-ellipsis line-clamp-3 overflow-hidden ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus, illo nem nihil vitae omnis alias illo exercitationem deserunt natus.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="md:w-1/2 w-full lg:p-4 md:p-3 p-2">
+                <div class="md:p-5 p-4  bg-white/5 backdrop-blur-sm rounded-[20px] border-[1px] border-solid border-[white]/10">
+                    <div class="flex items-center">
+                        <div>
+                            <div class="sm:w-14 sm:h-14  w-12 h-12 bg-[white]/10 border-[1px]  border-[white]/10 p-1 rounded-[23px]   flex items-center justify-center">   <img src="{{asset("img/Browser.svg")}}" alt="thanku" class="img-fluid w-100 h-100 z-10 "></div>
+                        </div>
+                        <div class="ms-3">
+                            <div class="text-white font-normal md:text-[20px] text-[18px]">Digital Marketing Services</div>
+                            <div class="text-white md:text-[15px] text-sm font-light mt-3  text-ellipsis line-clamp-3 overflow-hidden ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus, illo nem nihil vitae omnis alias illo exercitationem deserunt natus.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="md:w-1/2 w-full lg:p-4 md:p-3 p-2">
+                <div class="md:p-5 p-4  bg-white/5 backdrop-blur-sm rounded-[20px] border-[1px] border-solid border-[white]/10">
+                    <div class="flex items-center">
+                        <div>
+                            <div class="sm:w-14 sm:h-14  w-12 h-12 bg-[white]/10 border-[1px]  border-[white]/10 p-1 rounded-[23px]   flex items-center justify-center">   <img src="{{asset("img/Browser.svg")}}" alt="thanku" class="img-fluid w-100 h-100 z-10 "></div>
+                        </div>
+                        <div class="ms-3">
+                            <div class="text-white font-normal md:text-[20px] text-[18px]">Digital Marketing Services</div>
+                            <div class="text-white md:text-[15px] text-sm font-light mt-3  text-ellipsis line-clamp-3 overflow-hidden ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus, illo nem nihil vitae omnis alias illo exercitationem deserunt natus.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+       
+    </div>
+</div>
+
+
+<div class="relative overflow-hidden  bg-gradient-to-bl from-theme1 to to-theme2 py-10">
+    <div class="container mx-auto">
+        <div class="ps-4">
+            <div class="lg:text-6xl md:text-5xl sm:text-4xl text-3xl font-bold text-white">Lates From News</div>
+           
+        </div>
+        <div class="flex flex-row g-3 flex-wrap w-full mt-2 relative z-40">
+            <div class="md:w-1/2 w-full lg:p-4 md:p-3 p-2">
+                <div class="lg:p-10 md:p-8 p-5  bg-white/5 backdrop-blur-sm rounded-3xl border-[1px] border-solid border-[white]/10">
+                    <div class="flex items-center">
+                    
+                        <div class="">
+                            <div class="text-gray-300 text-sm font-light">Business</div>
+                            <div class="text-white font-semibold xl:text-3xl sm:text-2xl  text-xl leading-tight">Unleashing the power of Social Media Marketing</div>
+                            <div class="text-white md:text-[15px] text-sm font-light mt-3  text-ellipsis line-clamp-2 overflow-hidden ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus,orem ipsum dolor sit amet consectetur, adipisicing elit. Delectus, illo nem nihil vitae omnis alias illo exercitationem deserunt natus.</div>
+                            <div class="sm:mt-9 mt-5">
+                                <a href="javascript:;" class="text-decoration-none inline-flex text-white text-sm font-normal items-center md:px-20 px-16 md:py-4  py-3 rounded-[100px] bg-white/5 backdrop-blur-sm border-[1px] border-solid border-[white]/10">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="md:w-1/2 w-full lg:p-4 md:p-3 p-2">
+                <div class="lg:p-10 md:p-8 p-5  bg-white/5 backdrop-blur-sm rounded-3xl border-[1px] border-solid border-[white]/10">
+                    <div class="flex items-center">
+                    
+                        <div class="">
+                            <div class="text-gray-300 text-sm font-light">Business</div>
+                            <div class="text-white font-semibold xl:text-3xl sm:text-2xl  text-xl leading-tight">Unleashing the power of Social Media Marketing</div>
+                            <div class="text-white md:text-[15px] text-sm font-light mt-3  text-ellipsis line-clamp-2 overflow-hidden ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus,orem ipsum dolor sit amet consectetur, adipisicing elit. Delectus, illo nem nihil vitae omnis alias illo exercitationem deserunt natus.</div>
+                            <div class="mt-9">
+                                <a href="javascript:;" class="text-decoration-none inline-flex text-white text-sm font-normal items-center px-20 py-4 rounded-[100px] bg-white/5 backdrop-blur-sm border-[1px] border-solid border-[white]/10">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+       
+    </div>
+</div>
+
+
+<div class="overflow-hidden bg-[url('../../../../../../img/meeting.jpg')] bg-no-repeat bg-cover bg-center py-10 lg:h-screen relative after:absolute after:inset-0 after:bg-gradient-to-b after:from-blue-900/90 after:to-blue-950/90 after:mix-blend-multiply before:absolute before:inset-0 before:bg-gradint before:bg-gradient-to-b before:from-black/70 before:to-black/70 before:mix-blend-multiply">
+    <div class="container mx-auto relative z-20 h-full before:absolute before:w-52 before:mix-blend-screen before:h-52  before:bottom-0 before:rounded-full before:left-[-100px] before:bg-gradient-to-br before:from-pink-400/5 before:to-pink-600/5  after:absolute after:w-52 after:mix-blend-screen after:h-52  after:top-0 after:rounded-full after:right-[-100px] after:bg-gradient-to-br after:from-pink-400/5 after:to-pink-600/5">
+        <div class="flex   justify-center h-full flex-col">
+            <div class="ps-4">
+                <div class="text-white  font-bold xl:text-7xl lg:text-6xl md:text-5xl sm:text-4xl text-2xl"> Support Center</div>
+                <div class="text-white text-xl font-light leading-tight lg:w-1/2 lg:mt-7 mt-5">Lorem ipsum dolor sit, inventore, repellat quas architecto inventore, repellat quas architecto ratione accusamus nihil ullam.</div>
+            </div>
+            <div class="lg:mt-16 md-mt-12 mt-10">
+                <div class="flex flex-row g-3 flex-wrap w-full h-full">
+                  
+                    <div class="lg:w-1/3 md:w-1/2 lg:p-4 p-3">
+                        <div class=" bg-white/5 border-solid border-gray-500 border-[1px] rounded-xl lg:p-5 p-4 backdrop-blur-sm">
+                            <div class="flex ">
+                                <div>
+                                    <div class="sm:w-10 sm:h-10  w-8 h-8  flex items-center justify-center p-2">   <img src="{{asset("img/Browser.svg")}}" alt="thanku" class="img-fluid w-full h-full "></div>
+                                </div>
+                                <div class="ms-3">
+                                    <div class="text-white font-normal md:text-[20px] text-[18px]">Digital Marketing Services</div>
+                                    <div class="text-gray-300 md:text-base text-sm font-normal mt-2  text-ellipsis line-clamp-3 overflow-hidden ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus, illo nem nihil vitae omnis alias illo exercitationem deserunt natus.</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="lg:w-1/3 md:w-1/2 lg:p-4 p-3">
+                        <div class=" bg-white/5 border-solid border-gray-500 border-[1px] rounded-xl lg:p-5 p-4 backdrop-blur-sm">
+                            <div class="flex ">
+                                <div>
+                                    <div class="sm:w-10 sm:h-10  w-8 h-8  flex items-center justify-center p-2">   <img src="{{asset("img/Browser.svg")}}" alt="thanku" class="img-fluid w-full h-full "></div>
+                                </div>
+                                <div class="ms-3">
+                                    <div class="text-white font-normal md:text-[20px] text-[18px]">Digital Marketing Services</div>
+                                    <div class="text-gray-300 md:text-base text-sm font-normal mt-2  text-ellipsis line-clamp-3 overflow-hidden ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus, illo nem nihil vitae omnis alias illo exercitationem deserunt natus.</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="lg:w-1/3 md:w-1/2 lg:p-4 p-3">
+                        <div class=" bg-white/5 border-solid border-gray-500 border-[1px] rounded-xl lg:p-5 p-4 backdrop-blur-sm">
+                            <div class="flex ">
+                                <div>
+                                    <div class="sm:w-10 sm:h-10  w-8 h-8  flex items-center justify-center p-2">   <img src="{{asset("img/Browser.svg")}}" alt="thanku" class="img-fluid w-full h-full "></div>
+                                </div>
+                                <div class="ms-3">
+                                    <div class="text-white font-normal md:text-[20px] text-[18px]">Digital Marketing Services</div>
+                                    <div class="text-gray-300 md:text-base text-sm font-normal mt-2  text-ellipsis line-clamp-3 overflow-hidden ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus, illo nem nihil vitae omnis alias illo exercitationem deserunt natus.</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container mx-auto py-10">
+    <div class="flex flex-row">
+        <div class="w-1/4 p-3 flex-1 hover:flex-[3] duration-300">
+            <div class="relative bg-[url('../../../../../../img/case-study-1.jpg')] bg-no-repeat bg-cover bg-center h-80 rounded-xl overflow-hidden">
+                <div class="absolute inset-0 flex items-center justify-center">
+                    <div class="content">
+                        <div class="text-white text-2xl font-semibold">Content Management</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="w-1/4 p-3 flex-1 hover:flex-[3] duration-300">
+            <div class="relative bg-[url('../../../../../../img/case-study-1.jpg')] bg-no-repeat bg-cover bg-center h-80 rounded-xl overflow-hidden">
+                <div class="absolute"> hello</div>
+            </div>
+        </div>
+        <div class="w-1/4 p-3 flex-1 hover:flex-[3] duration-300">
+            <div class="relative bg-[url('../../../../../../img/case-study-1.jpg')] bg-no-repeat bg-cover bg-center h-80 rounded-xl overflow-hidden">
+                <div class="absolute"> hello</div>
+            </div>
+        </div>
+        <div class="w-1/4 p-3 flex-1 hover:flex-[3] duration-300">
+            <div class="relative bg-[url('../../../../../../img/case-study-1.jpg')] bg-no-repeat bg-cover bg-center h-80 rounded-xl overflow-hidden">
+                <div class="absolute"> hello</div>
             </div>
         </div>
     </div>
