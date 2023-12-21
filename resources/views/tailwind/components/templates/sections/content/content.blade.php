eef5eef7dd2543bc18f8ae50dda98caa116d6a65
mohitboharaimg <146337834+mohitboharaimg@users.noreply.github.com>
2023-12-21 18:23:27 +0530

    worked on why hire us section

--- resources/views/tailwind/components/templates/sections/content/content.blade.php
+++ resources/views/tailwind/components/templates/sections/content/content.blade.php
@@ -27,41 +27,34 @@
 
 
 @section('content')
-<div class="relative pt-16 bg-[#e8e9ec] md:pt-20 lg:pt-32">
-    <div class="text-black/10">
-        <svg aria-hidden="true" class="absolute inset-0 h-full w-full">
-            <defs>
-                <pattern id="t47" width="128" height="128" patternUnits="userSpaceOnUse" x="50%" patternTransform="translate(0 80)">
-                    <path d="M0 128V.5H128" fill="none" stroke="currentColor"></path>
-                </pattern>
-            </defs>
-            <rect width="100%" height="100%" fill="url(#t47)"></rect>
-        </svg>
+<div class="relative pt-16 md:pt-20 lg:pt-32">
+    <div class="text-blue-50">
+        <div class="absolute top-0 left-0 h-full w-full overflow-hidden"><svg class="w-auto h-[600px] fill-blue-50 opacity-70" viewBox="0 0 95 52.8"><path d="M27.3 0H0l26.4 26.4L0 52.8h27.3l26.4-26.4z"></path><path d="M68.6 0H41.3l26.4 26.4-26.4 26.4h27.3L95 26.4z"></path></svg></div>
     </div>
     <div class="mx-auto max-w-7xl px-6 lg:px-8 z-[1] relative py-20">
 
-<div class="py-xl-5 py-lg-5 py-md-5 py-5 relative">
+<div class="py-xl-5 py-lg-5 py-md-5 py-5 select-none relative">
     <div class="flex w-full h-auto items-center flex-wrap">
-        <div class="lg:w-[60%] pe-10 w-[100%] lg:order-1 order-2">
-            <p class="mt-4 select-none max-md:text-2xl/tight max-xl:text-3xl/tight text-4xl/tight tracking-[-0.04em]">
+        <div class="lg:w-[60%] lg:pe-10 pe-0 w-[100%] lg:order-1 order-2">
+            <p class="mt-4  max-md:text-2xl/tight max-xl:text-3xl/tight text-4xl/tight tracking-[-0.04em]">
                 <span class="font-normal">Why Hire Python Developers from <br></span> 
                 <span class="font-bold bg-gradient-to-r from-gray-500 via-sky-600 to-gray-500 bg-clip-text text-transparent"> IMG Global Infotech?</span>
             </p>
-            <div class="md:text-base text-sm opacity-70 mt-3 mb-lg-1 select-none">
+            <div class="md:text-base text-sm opacity-70 mt-3 mb-lg-1">
             Hiring a python developer from us would be the right choice because it will make your business thrive in on-going market trends. It may benefit for fast project development making sure all quality standards are matched. There are multiple reasons Python is preferable by developers. The python is highly readable and popular language amongst developer community due to numerous libraries easily accessible.
             </div> 
-            <div class="flex items-center mt-6 flex-wrap gap-3 w-[90%]  lg:w-full">
-                <div class="bg-white py-3 text-[#337ca6]  whitespace-nowrap font-semibold text-sm shadow-slate-300 shadow-lg rounded-md px-4">8+ Years In Busines</div>
-                <div class="bg-white py-3 text-[#337ca6]  whitespace-nowrap font-semibold text-sm shadow-slate-300 shadow-lg rounded-md px-4">Experienced And Talented Coders</div>
-                <div class="bg-white py-3 text-[#337ca6]  whitespace-nowrap font-semibold text-sm shadow-slate-300 shadow-lg rounded-md px-4">Cost-Effective Solutions</div>
-                <div class="bg-white py-3 text-[#337ca6]  whitespace-nowrap font-semibold text-sm shadow-slate-300 shadow-lg rounded-md px-4">
+            <div class="flex items-center mt-6 flex-wrap gap-3 w-full  lg:w-[90%]">
+                <div class="bg-white lg:py-3 py-2 text-[#337ca6]  whitespace-nowrap font-semibold text-sm shadow-slate-200 shadow-md  px-3 lg:px-4">8+ Years In Busines</div>
+                <div class="bg-white lg:py-3 py-2 text-[#337ca6]  whitespace-nowrap font-semibold text-sm shadow-slate-200 shadow-md  px-3 lg:px-4">Experienced And Talented Coders</div>
+                <div class="bg-white lg:py-3 py-2 text-[#337ca6]  whitespace-nowrap font-semibold text-sm shadow-slate-200 shadow-md  px-3 lg:px-4">Cost-Effective Solutions</div>
+                <div class="bg-white lg:py-3 py-2 text-[#337ca6]  whitespace-nowrap font-semibold text-sm shadow-slate-200 shadow-md  px-3 lg:px-4">
                     Clear Communication</div>
-                <div class="bg-white py-3 text-[#337ca6]  whitespace-nowrap font-semibold text-sm shadow-slate-300 shadow-lg rounded-md px-4">Transparency Guaranteed</div>
-                <div class="bg-white py-3 text-[#337ca6]  whitespace-nowrap font-semibold text-sm shadow-slate-300 shadow-lg rounded-md px-4">100+ In-House Developers</div>
-                <div class="bg-white py-3 text-[#337ca6]  whitespace-nowrap font-semibold text-sm shadow-slate-300 shadow-lg rounded-md px-4">Flexible Engagement Models</div>
-                <div class="bg-white py-3 text-[#337ca6]  whitespace-nowrap font-semibold text-sm shadow-slate-300 shadow-lg rounded-md px-4">Daily, Weekly, And Monthly Reporting</div>
-                <div class="bg-white py-3 text-[#337ca6]  whitespace-nowrap font-semibold text-sm shadow-slate-300 shadow-lg rounded-md px-4">On-Time Efficiency</div>
-                <div class="bg-white py-3 text-[#337ca6]  whitespace-nowrap font-semibold text-sm shadow-slate-300 shadow-lg rounded-md px-4">Data-Driven Algorithms
+                <div class="bg-white lg:py-3 py-2 text-[#337ca6]  whitespace-nowrap font-semibold text-sm shadow-slate-200 shadow-md  px-3 lg:px-4">Transparency Guaranteed</div>
+                <div class="bg-white lg:py-3 py-2 text-[#337ca6]  whitespace-nowrap font-semibold text-sm shadow-slate-200 shadow-md  px-3 lg:px-4">100+ In-House Developers</div>
+                <div class="bg-white lg:py-3 py-2 text-[#337ca6]  whitespace-nowrap font-semibold text-sm shadow-slate-200 shadow-md  px-3 lg:px-4">Flexible Engagement Models</div>
+                <div class="bg-white lg:py-3 py-2 text-[#337ca6]  whitespace-nowrap font-semibold text-sm shadow-slate-200 shadow-md  px-3 lg:px-4">Daily, Weekly, And Monthly Reporting</div>
+                <div class="bg-white lg:py-3 py-2 text-[#337ca6]  whitespace-nowrap font-semibold text-sm shadow-slate-200 shadow-md  px-3 lg:px-4">On-Time Efficiency</div>
+                <div class="bg-white lg:py-3 py-2 text-[#337ca6]  whitespace-nowrap font-semibold text-sm shadow-slate-200 shadow-md  px-3 lg:px-4">Data-Driven Algorithms
                 </div>
             </div>
         </div>
@@ -73,6 +66,22 @@
         </div>
 </div>
 
+<div class="w-full flex items-center justify-between pt-4">
+<div class="w-[40%]">
+<div class="aspect-[3/2.8] relative z-10">
+    <img src="{{asset('public'.mix('/tailwind/img/fabindia.webp'))}}" alt="thanku" class="img-fluid h-full w-full">
+</div>
+</div>
+
+<div class="w-60%">
+    <div class="text-[#337ca6] text-sm font-semibold mx-auto">Touching Lives Through Technology</div>
+    <p class="mt-4  max-md:text-2xl/tight max-xl:text-3xl/tight text-4xl/tight tracking-[-0.04em]">
+        <span class="font-normal">Why Hire Python Developers from <br></span> 
+        <span class="font-bold bg-gradient-to-r from-gray-500 via-sky-600 to-gray-500 bg-clip-text text-transparent"> IMG Global Infotech?</span>
+    </p>
+</div>
+    </div>
+</div>
 
 
 
