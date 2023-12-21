130c7d917fb1ae3fccde266758da575ded37b2ac
shubham agrawal <145680136+shubhamimg@users.noreply.github.com>
2023-12-21 14:27:08 +0530

    appdevelopment HomeSection

--- resources/views/tailwind/pages/home-delivery-app-development/home-delivery-app-development.blade.php
+++ resources/views/tailwind/pages/home-delivery-app-development/home-delivery-app-development.blade.php
@@ -39,29 +39,69 @@
 
 <div class="relative pt-16 md:pt-20 lg:pt-32 select-none">
     <div class="text-black/10">
-        <svg aria-hidden="true" class="absolute inset-0 h-full w-full">
-            <defs>
-                <pattern id="t47" width="128" height="128" patternUnits="userSpaceOnUse" x="50%" patternTransform="translate(0 80)">
-                    <path d="M0 128V.5H128" fill="none" stroke="currentColor"></path>
-                </pattern>
-            </defs>
-            <rect width="100%" height="100%" fill="url(#t47)"></rect>
+        <svg xmlns='http://www.w3.org/2000/svg' width='26' height='26' viewBox="0 0 8 8">
+            <circle cx="1" cy="1" r="1" opacity='0.25' />
+            <circle cx="5" cy="5" r="1" opacity='0.25' />
         </svg>
     </div>
-    <div class="mx-auto max-w-7xl px-6 lg:px-8 z-[1] relative py-20">
+    <div class="mx-auto max-w-7xl px-6 lg:pl-48 lg:pr-52 z-[1] relative py-20 text-center">
 
 
-        <p class="max-md:text-2xl/tight max-xl:text-3xl/tight text-4xl/tight tracking-[-0.04em] text-black pb-8">
-            <span class="font-normal">Disclaimer - </span> 
-            <span class="font-bold bg-gradient-to-r from-gray-500 via-sky-600 to-gray-500 bg-clip-text text-transparent"> IMG Global Infotech</span>
-        </p>
-        <div class="w-full text-sm md:text-base text-gray-600 flex flex-col gap-y-4">
-            <div>The information contained in this website is for general information purposes only. The information is provided by <b>IMG GLOBAL INFOTECH</b> and while we endeavour to keep the information up to date and correct, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.</div> 
-            <div>In no event will we be liable for any loss or damage including without limitation, indirect or consequential loss or damage, or any loss or damage whatsoever arising from loss of data or profits arising out of, or in connection with, the use of this website.
-                Through this website you are able to link to other websites which are not under the control of <b>IMG GLOBAL INFOTECH</b> We have no control over the nature, content and availability of those sites. The inclusion of any links does not necessarily imply a recommendation or endorse the views expressed within them.
-                <b>IMG Global Infotech</b> does not intend to infringe the copyright of any outlet by using images and content. For any copyright-related issues right to us and we would reply within 48 Hours.</div> 
+        <div class="text-2xl mb-3 ">
+            Build Online Food Delivery Apps and Unlock 67% Savings
         </div>
-
+        <div class="heading text-6xl font-bold text-[#111457]  leading-[120%] mb-5">
+            On-Demand Food Delivery App Development Company
+        </div>
+        <div class=" mt-3 mb-5">
+            iCoderz Solutions is the best Food delivery app development company with over 12 years of experience serving food business owners. Our <span class="font-bold">on demand food delivery app development</span> services help food business owners increase their brand visibility and sales rapidly.
+        </div>
+        <div class="mt-8 flex items-center justify-around">
+            <div class="flex items-center gap-3">
+                <div class="icon h-[25px] w-[25px]">
+                    <img src="{{asset('public'.mix('/tailwind/img/rightBlue.svg'))}}" alt="thanku" class="img-fluid h-full w-full ">
+                </div>
+                <div class="text-lg font-bold text-[#111457]">Get solution Delivered in 2 week</div>
+            </div>
+            <div class="flex items-center gap-3 ">
+                <div class="icon h-[25px] w-[25px]">
+                    <img src="{{asset('public'.mix('/tailwind/img/rightBlue.svg'))}}" alt="thanku" class="img-fluid h-full w-full ">
+                </div>
+                <div class="text-lg font-bold text-[#111457]">Dedicated team of food delivery App Developer</div>
+            </div>
+        </div>
+        <div class="mt-4 flex items-center justify-around">
+            <div class="flex items-center gap-3">
+                <div class="icon h-[25px] w-[25px]">
+                    <img src="{{asset('public'.mix('/tailwind/img/rightBlue.svg'))}}" alt="thanku" class="img-fluid h-full w-full ">
+                </div>
+                <div class="text-lg font-bold text-[#111457]">Proven Expertise & Cost Effective</div>
+            </div>
+            <div class="flex items-center gap-3 ">
+                <div class="icon h-[25px] w-[25px]">
+                    <img src="{{asset('public'.mix('/tailwind/img/rightBlue.svg'))}}" alt="thanku" class="img-fluid h-full w-full ">
+                </div>
+                <div class="text-lg font-bold text-[#111457]">100% customizable solutions</div>
+            </div>
+        </div>
+        <div class="mt-4 flex items-center justify-around">
+            <div class="flex items-center gap-3">
+                <div class="icon h-[25px] w-[25px]">
+                    <img src="{{asset('public'.mix('/tailwind/img/rightBlue.svg'))}}" alt="thanku" class="img-fluid h-full w-full ">
+                </div>
+                <div class="text-lg font-bold text-[#111457]">Fully Signed NDA & Code Security</div>
+            </div>
+        </div>
+        <div class="absolute top-[-2%] left-[-3%] h-[220px] w-[249px]">
+            <img src="{{asset('public'.mix('/tailwind/img/pizzaShimg.png'))}}" alt="thanku" class="img-fluid h-full w-full ">
+        </div>
+        <div class="absolute bottom-[-1%] right-[-3%] h-[220px] w-[249px]">
+            <img src="{{asset('public'.mix('/tailwind/img/burgerShImg.png'))}}" alt="thanku" class="img-fluid h-full w-full ">
+        </div>
+        <div class=" mt-10">
+            <a href="https://www.imgglobalinfotech.com/about-us.php" class="inline-flex rounded-br-none justify-center rounded-lg text-sm font-semibold py-2 md:py-3 px-3 md:px-4 bg-slate-900 text-white hover:bg-slate-700"><span class="select-none">Get Your Free Quote <span aria-hidden="true" class="text-slate-400 sm:inline">→</span></span></a>
+        </div>
+        <div class=" mt-3">All our projects are secured by NDA</div>
 
 
 
