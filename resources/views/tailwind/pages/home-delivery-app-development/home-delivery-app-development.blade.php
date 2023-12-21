942a3ae3af66af08af4f2a4152d695b15b202b24
shubham agrawal <145680136+shubhamimg@users.noreply.github.com>
2023-12-21 18:06:08 +0530

    food delivery development final

--- resources/views/tailwind/pages/home-delivery-app-development/home-delivery-app-development.blade.php
+++ resources/views/tailwind/pages/home-delivery-app-development/home-delivery-app-development.blade.php
@@ -37,71 +37,70 @@
 
 
 
-<div class="relative pt-16 md:pt-20 lg:pt-32 select-none">
+<div class="relative pt-16 md:pt-20 lg:pt-32 select-none overflow-hidden">
     <div class="text-black/10">
-        <svg xmlns='http://www.w3.org/2000/svg' width='26' height='26' viewBox="0 0 8 8">
-            <circle cx="1" cy="1" r="1" opacity='0.25' />
-            <circle cx="5" cy="5" r="1" opacity='0.25' />
-        </svg>
+        <svg xmlns='http://www.w3.org/2000/svg' width='100%' class="absolute inset-0 h-full w-full"><rect fill='#ffffff' width='540' height='450'/><defs><linearGradient id='a' gradientUnits='userSpaceOnUse' x1='0' x2='0' y1='0' y2='100%' gradientTransform='rotate(240)'><stop offset='0'  stop-color='#ffffff'/><stop offset='1'  stop-color='#FAF7F6'/></linearGradient><pattern patternUnits='userSpaceOnUse' id='b'  width='300' height='250' x='0' y='0' viewBox='0 0 1080 900'><g fill-opacity='0.04'><polygon fill='#444' points='90 150 0 300 180 300'/><polygon points='90 150 180 0 0 0'/><polygon fill='#AAA' points='270 150 360 0 180 0'/><polygon fill='#DDD' points='450 150 360 300 540 300'/><polygon fill='#999' points='450 150 540 0 360 0'/><polygon points='630 150 540 300 720 300'/><polygon fill='#DDD' points='630 150 720 0 540 0'/><polygon fill='#444' points='810 150 720 300 900 300'/><polygon fill='#FFF' points='810 150 900 0 720 0'/><polygon fill='#DDD' points='990 150 900 300 1080 300'/><polygon fill='#444' points='990 150 1080 0 900 0'/><polygon fill='#DDD' points='90 450 0 600 180 600'/><polygon points='90 450 180 300 0 300'/><polygon fill='#666' points='270 450 180 600 360 600'/><polygon fill='#AAA' points='270 450 360 300 180 300'/><polygon fill='#DDD' points='450 450 360 600 540 600'/><polygon fill='#999' points='450 450 540 300 360 300'/><polygon fill='#999' points='630 450 540 600 720 600'/><polygon fill='#FFF' points='630 450 720 300 540 300'/><polygon points='810 450 720 600 900 600'/><polygon fill='#DDD' points='810 450 900 300 720 300'/><polygon fill='#AAA' points='990 450 900 600 1080 600'/><polygon fill='#444' points='990 450 1080 300 900 300'/><polygon fill='#222' points='90 750 0 900 180 900'/><polygon points='270 750 180 900 360 900'/><polygon fill='#DDD' points='270 750 360 600 180 600'/><polygon points='450 750 540 600 360 600'/><polygon points='630 750 540 900 720 900'/><polygon fill='#444' points='630 750 720 600 540 600'/><polygon fill='#AAA' points='810 750 720 900 900 900'/><polygon fill='#666' points='810 750 900 600 720 600'/><polygon fill='#999' points='990 750 900 900 1080 900'/><polygon fill='#999' points='180 0 90 150 270 150'/><polygon fill='#444' points='360 0 270 150 450 150'/><polygon fill='#FFF' points='540 0 450 150 630 150'/><polygon points='900 0 810 150 990 150'/><polygon fill='#222' points='0 300 -90 450 90 450'/><polygon fill='#FFF' points='0 300 90 150 -90 150'/><polygon fill='#FFF' points='180 300 90 450 270 450'/><polygon fill='#666' points='180 300 270 150 90 150'/><polygon fill='#222' points='360 300 270 450 450 450'/><polygon fill='#FFF' points='360 300 450 150 270 150'/><polygon fill='#444' points='540 300 450 450 630 450'/><polygon fill='#222' points='540 300 630 150 450 150'/><polygon fill='#AAA' points='720 300 630 450 810 450'/><polygon fill='#666' points='720 300 810 150 630 150'/><polygon fill='#FFF' points='900 300 810 450 990 450'/><polygon fill='#999' points='900 300 990 150 810 150'/><polygon points='0 600 -90 750 90 750'/><polygon fill='#666' points='0 600 90 450 -90 450'/><polygon fill='#AAA' points='180 600 90 750 270 750'/><polygon fill='#444' points='180 600 270 450 90 450'/><polygon fill='#444' points='360 600 270 750 450 750'/><polygon fill='#999' points='360 600 450 450 270 450'/><polygon fill='#666' points='540 600 630 450 450 450'/><polygon fill='#222' points='720 600 630 750 810 750'/><polygon fill='#FFF' points='900 600 810 750 990 750'/><polygon fill='#222' points='900 600 990 450 810 450'/><polygon fill='#DDD' points='0 900 90 750 -90 750'/><polygon fill='#444' points='180 900 270 750 90 750'/><polygon fill='#FFF' points='360 900 450 750 270 750'/><polygon fill='#AAA' points='540 900 630 750 450 750'/><polygon fill='#FFF' points='720 900 810 750 630 750'/><polygon fill='#222' points='900 900 990 750 810 750'/><polygon fill='#222' points='1080 300 990 450 1170 450'/><polygon fill='#FFF' points='1080 300 1170 150 990 150'/><polygon points='1080 600 990 750 1170 750'/><polygon fill='#666' points='1080 600 1170 450 990 450'/><polygon fill='#DDD' points='1080 900 1170 750 990 750'/></g></pattern></defs><rect x='0' y='0' fill='url(#a)' width='100%' height='100%'/><rect x='0' y='0' fill='url(#b)' width='100%' height='100%'/></svg>
     </div>
-    <div class="mx-auto max-w-7xl px-6 lg:pl-48 lg:pr-52 z-[1] relative py-20 text-center">
+    <div class="mx-auto max-w-7xl px-6 lg:pl-48 lg:pr-52 z-[1] relative py-8 sm:py-10 md:py-14 lg:py-20 text-center">
 
 
-        <div class="text-2xl mb-3 ">
+        <div class="text-xl md:text-2xl mb-3">
             Build Online Food Delivery Apps and Unlock 67% Savings
         </div>
-        <div class="heading text-6xl font-bold text-[#111457]  leading-[120%] mb-5">
+        <div class="heading text-2xl sm:text-3xl md:text-4xl lg:text-6xl font-bold text-[#111457]  leading-[120%] mb-5">
             On-Demand Food Delivery App Development Company
         </div>
-        <div class=" mt-3 mb-5">
+        <div class=" mt-3 mb-5 text-sm md:text-base">
             iCoderz Solutions is the best Food delivery app development company with over 12 years of experience serving food business owners. Our <span class="font-bold">on demand food delivery app development</span> services help food business owners increase their brand visibility and sales rapidly.
         </div>
-        <div class="mt-8 flex items-center justify-around">
+        <div class="mt-8 flex items-center flex-wrap gap-y-3 gap-3 md:justify-around">
             <div class="flex items-center gap-3">
-                <div class="icon h-[25px] w-[25px]">
+                <div class="icon h-[16px] sm:h-[20px] md:h-[25px]  w-[16px] sm:w-[20px] md:w-[25px]">
                     <img src="{{asset('public'.mix('/tailwind/img/rightBlue.svg'))}}" alt="thanku" class="img-fluid h-full w-full ">
                 </div>
-                <div class="text-lg font-bold text-[#111457]">Get solution Delivered in 2 week</div>
+                <div class="text-[12px] sm:text-base md:text-lg font-bold text-[#111457]">Get solution Delivered in 2 week</div>
             </div>
             <div class="flex items-center gap-3 ">
-                <div class="icon h-[25px] w-[25px]">
+                <div class="icon h-[16px] sm:h-[20px] md:h-[25px]  w-[16px] sm:w-[20px] md:w-[25px]">
                     <img src="{{asset('public'.mix('/tailwind/img/rightBlue.svg'))}}" alt="thanku" class="img-fluid h-full w-full ">
                 </div>
-                <div class="text-lg font-bold text-[#111457]">Dedicated team of food delivery App Developer</div>
+                <div class="text-[12px] sm:text-base md:text-lg font-bold text-[#111457]">Dedicated team of food delivery App Developer</div>
             </div>
         </div>
-        <div class="mt-4 flex items-center justify-around">
+        <div class="mt-4 flex items-center flex-wrap gap-y-3 gap-3 md:justify-around">
             <div class="flex items-center gap-3">
-                <div class="icon h-[25px] w-[25px]">
+                <div class="icon h-[16px] sm:h-[20px] md:h-[25px]  w-[16px] sm:w-[20px] md:w-[25px]">
                     <img src="{{asset('public'.mix('/tailwind/img/rightBlue.svg'))}}" alt="thanku" class="img-fluid h-full w-full ">
                 </div>
-                <div class="text-lg font-bold text-[#111457]">Proven Expertise & Cost Effective</div>
+                <div class="text-[12px] sm:text-base md:text-lg font-bold text-[#111457]">Proven Expertise & Cost Effective</div>
             </div>
             <div class="flex items-center gap-3 ">
-                <div class="icon h-[25px] w-[25px]">
+                <div class="icon h-[16px] sm:h-[20px] md:h-[25px]  w-[16px] sm:w-[20px] md:w-[25px]">
                     <img src="{{asset('public'.mix('/tailwind/img/rightBlue.svg'))}}" alt="thanku" class="img-fluid h-full w-full ">
                 </div>
-                <div class="text-lg font-bold text-[#111457]">100% customizable solutions</div>
+                <div class="text-[12px] sm:text-base md:text-lg font-bold text-[#111457]">100% customizable solutions</div>
             </div>
         </div>
-        <div class="mt-4 flex items-center justify-around">
+        <div class="mt-4 flex items-center flex-wrap gap-y-3 gap-3 md:justify-around">
             <div class="flex items-center gap-3">
-                <div class="icon h-[25px] w-[25px]">
+                <div class="icon h-[16px] sm:h-[20px] md:h-[25px]  w-[16px] sm:w-[20px] md:w-[25px]">
                     <img src="{{asset('public'.mix('/tailwind/img/rightBlue.svg'))}}" alt="thanku" class="img-fluid h-full w-full ">
                 </div>
-                <div class="text-lg font-bold text-[#111457]">Fully Signed NDA & Code Security</div>
+                <div class="text-[12px] sm:text-base md:text-lg font-bold text-[#111457]">Fully Signed NDA & Code Security</div>
             </div>
         </div>
-        <div class="absolute top-[-2%] left-[-3%] h-[220px] w-[249px]">
+        <div class="absolute hidden lg:block top-[-2%] left-[-3%] h-[220px] w-[249px]">
             <img src="{{asset('public'.mix('/tailwind/img/pizzaShimg.png'))}}" alt="thanku" class="img-fluid h-full w-full ">
         </div>
-        <div class="absolute bottom-[-1%] right-[-3%] h-[220px] w-[249px]">
+        <div class="absolute hidden lg:block bottom-[-1%] right-[-34px] h-[175px] w-[240px]">
             <img src="{{asset('public'.mix('/tailwind/img/burgerShImg.png'))}}" alt="thanku" class="img-fluid h-full w-full ">
         </div>
         <div class=" mt-10">
             <a href="https://www.imgglobalinfotech.com/about-us.php" class="inline-flex rounded-br-none justify-center rounded-lg text-sm font-semibold py-2 md:py-3 px-3 md:px-4 bg-slate-900 text-white hover:bg-slate-700"><span class="select-none">Get Your Free Quote <span aria-hidden="true" class="text-slate-400 sm:inline">→</span></span></a>
         </div>
-        <div class=" mt-3">All our projects are secured by NDA</div>
+        <div class="text-sm md:text-base mt-3">
+            All our projects are secured by NDA
+        </div>
 
 
 
