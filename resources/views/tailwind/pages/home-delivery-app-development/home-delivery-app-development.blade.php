a2318319f48b956b6f6dddcd32183e503ab0a615
shubham agrawal <145680136+shubhamimg@users.noreply.github.com>
2023-12-21 11:50:00 +0530

    Create home-delivery-app-development.blade.php

---
+++ resources/views/tailwind/pages/home-delivery-app-development/home-delivery-app-development.blade.php
@@ -0,0 +1,77 @@
+@extends('tailwind.index2')
+@push('meta')
+<!--  Title -->
+<title>Disclaimer - IMG Global Infotech</title>
+
+<!-- Required meta tags -->
+<meta name="title" content="Disclaimer - IMG Global Infotech">
+<meta name="description" content="The information contained in this website is for general information purposes only. The information is provided by IMG Global Infotech and while we endeavour to keep the information up to date and correct, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.">
+<meta name="keywords" content="IMG Global Infotech, Disclaimer">
+
+<!-- Twitter Meta -->
+<meta name="twitter:title" content="Disclaimer - IMG Global Infotech">
+<meta name="twitter:description" content="The information contained in this website is for general information purposes only. The information is provided by IMG Global Infotech and while we endeavour to keep the information up to date and correct, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.">
+
+
+<!-- Facebook Meta -->
+<meta property="og:url" content="{{asset('/')}}disclaimer.php">
+<meta property="og:title" content="Disclaimer - IMG Global Infotech">
+<meta property="og:description" content="The information contained in this website is for general information purposes only. The information is provided by IMG Global Infotech and while we endeavour to keep the infordisclaimer/mation up to date and correct, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.">
+
+
+
+
+
+<meta name="classification" content="IMG Global Infotech, Disclaimer">
+<link rel="canonical" href="{{asset('/')}}disclaimer.php">
+<meta name="robots" content="index, follow">
+@endpush
+
+@push('styles')
+@endpush
+
+@section('content')
+<div class="hidden">
+    <h1>Disclaimer</h1> 
+</div>
+
+
+
+<div class="relative pt-16 md:pt-20 lg:pt-32 select-none">
+    <div class="text-black/10">
+        <svg aria-hidden="true" class="absolute inset-0 h-full w-full">
+            <defs>
+                <pattern id="t47" width="128" height="128" patternUnits="userSpaceOnUse" x="50%" patternTransform="translate(0 80)">
+                    <path d="M0 128V.5H128" fill="none" stroke="currentColor"></path>
+                </pattern>
+            </defs>
+            <rect width="100%" height="100%" fill="url(#t47)"></rect>
+        </svg>
+    </div>
+    <div class="mx-auto max-w-7xl px-6 lg:px-8 z-[1] relative py-10 shadow-md shadow-sky-950/20 border-2 bg-white my-10 rounded-xl">
+
+
+        <p class="max-md:text-2xl/tight max-xl:text-3xl/tight text-4xl/tight tracking-[-0.04em] text-black pb-8">
+            <span class="font-normal">Disclaimer - </span> 
+            <span class="font-bold bg-gradient-to-r from-gray-500 via-sky-600 to-gray-500 bg-clip-text text-transparent"> IMG Global Infotech</span>
+        </p>
+        <div class="w-full text-sm md:text-base text-gray-600 flex flex-col gap-y-4">
+            <div>The information contained in this website is for general information purposes only. The information is provided by <b>IMG GLOBAL INFOTECH</b> and while we endeavour to keep the information up to date and correct, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.</div> 
+            <div>In no event will we be liable for any loss or damage including without limitation, indirect or consequential loss or damage, or any loss or damage whatsoever arising from loss of data or profits arising out of, or in connection with, the use of this website.
+                Through this website you are able to link to other websites which are not under the control of <b>IMG GLOBAL INFOTECH</b> We have no control over the nature, content and availability of those sites. The inclusion of any links does not necessarily imply a recommendation or endorse the views expressed within them.
+                <b>IMG Global Infotech</b> does not intend to infringe the copyright of any outlet by using images and content. For any copyright-related issues right to us and we would reply within 48 Hours.</div> 
+        </div>
+
+
+
+
+        
+    </div>
+</div>
+
+
+
+@endsection
+
+@push('scripts')
+@endpush
