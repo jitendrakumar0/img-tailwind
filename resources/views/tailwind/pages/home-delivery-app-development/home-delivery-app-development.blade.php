0ebb61d4555ce7af53b6667629e1b0de77cc5cfe
Jitendra Kumar Bijarnia <135127037+shivtejimg@users.noreply.github.com>
2023-12-22 15:02:34 +0530

    why choose section code marge mohit sir

--- resources/views/tailwind/pages/home-delivery-app-development/home-delivery-app-development.blade.php
+++ resources/views/tailwind/pages/home-delivery-app-development/home-delivery-app-development.blade.php
@@ -36,54 +36,63 @@
     <div class="text-black/10">
         <svg xmlns='http://www.w3.org/2000/svg' width='100%' class="absolute inset-0 h-full w-full"><rect fill='#ffffff' width='540' height='450'/><defs><linearGradient id='a' gradientUnits='userSpaceOnUse' x1='0' x2='0' y1='0' y2='100%' gradientTransform='rotate(240)'><stop offset='0'  stop-color='#ffffff'/><stop offset='1'  stop-color='#FAF7F6'/></linearGradient><pattern patternUnits='userSpaceOnUse' id='b'  width='300' height='250' x='0' y='0' viewBox='0 0 1080 900'><g fill-opacity='0.04'><polygon fill='#444' points='90 150 0 300 180 300'/><polygon points='90 150 180 0 0 0'/><polygon fill='#AAA' points='270 150 360 0 180 0'/><polygon fill='#DDD' points='450 150 360 300 540 300'/><polygon fill='#999' points='450 150 540 0 360 0'/><polygon points='630 150 540 300 720 300'/><polygon fill='#DDD' points='630 150 720 0 540 0'/><polygon fill='#444' points='810 150 720 300 900 300'/><polygon fill='#FFF' points='810 150 900 0 720 0'/><polygon fill='#DDD' points='990 150 900 300 1080 300'/><polygon fill='#444' points='990 150 1080 0 900 0'/><polygon fill='#DDD' points='90 450 0 600 180 600'/><polygon points='90 450 180 300 0 300'/><polygon fill='#666' points='270 450 180 600 360 600'/><polygon fill='#AAA' points='270 450 360 300 180 300'/><polygon fill='#DDD' points='450 450 360 600 540 600'/><polygon fill='#999' points='450 450 540 300 360 300'/><polygon fill='#999' points='630 450 540 600 720 600'/><polygon fill='#FFF' points='630 450 720 300 540 300'/><polygon points='810 450 720 600 900 600'/><polygon fill='#DDD' points='810 450 900 300 720 300'/><polygon fill='#AAA' points='990 450 900 600 1080 600'/><polygon fill='#444' points='990 450 1080 300 900 300'/><polygon fill='#222' points='90 750 0 900 180 900'/><polygon points='270 750 180 900 360 900'/><polygon fill='#DDD' points='270 750 360 600 180 600'/><polygon points='450 750 540 600 360 600'/><polygon points='630 750 540 900 720 900'/><polygon fill='#444' points='630 750 720 600 540 600'/><polygon fill='#AAA' points='810 750 720 900 900 900'/><polygon fill='#666' points='810 750 900 600 720 600'/><polygon fill='#999' points='990 750 900 900 1080 900'/><polygon fill='#999' points='180 0 90 150 270 150'/><polygon fill='#444' points='360 0 270 150 450 150'/><polygon fill='#FFF' points='540 0 450 150 630 150'/><polygon points='900 0 810 150 990 150'/><polygon fill='#222' points='0 300 -90 450 90 450'/><polygon fill='#FFF' points='0 300 90 150 -90 150'/><polygon fill='#FFF' points='180 300 90 450 270 450'/><polygon fill='#666' points='180 300 270 150 90 150'/><polygon fill='#222' points='360 300 270 450 450 450'/><polygon fill='#FFF' points='360 300 450 150 270 150'/><polygon fill='#444' points='540 300 450 450 630 450'/><polygon fill='#222' points='540 300 630 150 450 150'/><polygon fill='#AAA' points='720 300 630 450 810 450'/><polygon fill='#666' points='720 300 810 150 630 150'/><polygon fill='#FFF' points='900 300 810 450 990 450'/><polygon fill='#999' points='900 300 990 150 810 150'/><polygon points='0 600 -90 750 90 750'/><polygon fill='#666' points='0 600 90 450 -90 450'/><polygon fill='#AAA' points='180 600 90 750 270 750'/><polygon fill='#444' points='180 600 270 450 90 450'/><polygon fill='#444' points='360 600 270 750 450 750'/><polygon fill='#999' points='360 600 450 450 270 450'/><polygon fill='#666' points='540 600 630 450 450 450'/><polygon fill='#222' points='720 600 630 750 810 750'/><polygon fill='#FFF' points='900 600 810 750 990 750'/><polygon fill='#222' points='900 600 990 450 810 450'/><polygon fill='#DDD' points='0 900 90 750 -90 750'/><polygon fill='#444' points='180 900 270 750 90 750'/><polygon fill='#FFF' points='360 900 450 750 270 750'/><polygon fill='#AAA' points='540 900 630 750 450 750'/><polygon fill='#FFF' points='720 900 810 750 630 750'/><polygon fill='#222' points='900 900 990 750 810 750'/><polygon fill='#222' points='1080 300 990 450 1170 450'/><polygon fill='#FFF' points='1080 300 1170 150 990 150'/><polygon points='1080 600 990 750 1170 750'/><polygon fill='#666' points='1080 600 1170 450 990 450'/><polygon fill='#DDD' points='1080 900 1170 750 990 750'/></g></pattern></defs><rect x='0' y='0' fill='url(#a)' width='100%' height='100%'/><rect x='0' y='0' fill='url(#b)' width='100%' height='100%'/></svg>
     </div>
-    <div class="mx-auto max-w-7xl px-6 lg:pl-48 lg:pr-52 z-[1] relative py-8 sm:py-10 md:py-14 lg:py-20 text-center">
+    <div class="mx-auto max-w-7xl px-6 lg:pl-48 lg:pr-52 z-[1] relative py-8 sm:py-10 md:py-14 lg:py-16 text-center">
 
 
-        <div class="text-xl md:text-2xl mb-3">
+        {{-- <div class="text-xl md:text-2xl mb-3">
             Build Online Food Delivery Apps and Unlock 67% Savings
+        </div> --}}
+        <div class="md:pt-3 relative container mx-auto max-w-7xl md:px-6 lg:px-8 z-10">
+            <div class="md:text-base text-sm text-black/70 mt-3 mb-lg-1">
+                Build Online Food Delivery Apps and Unlock 67% Savings
+            </div>
+
+            <p class="mt-4 max-md:text-2xl/tight max-xl:text-3xl/tight text-4xl/tight tracking-[-0.04em] text-black">
+                <span class="font-bold"> On-Demand Food Delivery</span> 
+                <span class="font-bold bg-gradient-to-r from-gray-500 via-sky-600 to-gray-500 bg-clip-text text-transparent"> App Development Company</span>
+            </p>
+            <div class="md:text-base text-sm text-black/70 mt-3 mb-lg-1">
+                iCoderz Solutions is the best Food delivery app development company with over 12 years of experience serving food business owners. Our <span class="font-bold">on demand food delivery app development</span> services help food business owners increase their brand visibility and sales rapidly.
+            </div>
         </div>
-        <div class="heading text-2xl sm:text-3xl md:text-4xl lg:text-6xl font-bold text-[#111457]  leading-[120%] mb-5">
-            On-Demand Food Delivery App Development Company
-        </div>
-        <div class=" mt-3 mb-5 text-sm md:text-base">
+        {{-- <div class=" mt-3 mb-5 text-sm md:text-base">
             iCoderz Solutions is the best Food delivery app development company with over 12 years of experience serving food business owners. Our <span class="font-bold">on demand food delivery app development</span> services help food business owners increase their brand visibility and sales rapidly.
-        </div>
-        <div class="mt-8 flex items-center flex-wrap gap-y-3 gap-3 md:justify-around">
+        </div> --}}
+        <div class="mt-8 flex items-center flex-wrap gap-y-3 gap-3 md:justify-center mx-auto">
             <div class="flex items-center gap-3">
-                <div class="icon h-[16px] sm:h-[20px] md:h-[25px]  w-[16px] sm:w-[20px] md:w-[25px]">
+                <div class="icon h-[14px] sm:h-[20px] md:h-[18px]  w-[14px] sm:w-[14px] md:w-[18px]">
                     <img src="{{asset('public'.mix('/tailwind/img/rightBlue.svg'))}}" alt="thanku" class="img-fluid h-full w-full ">
                 </div>
-                <div class="text-[12px] sm:text-base md:text-lg font-bold text-[#111457]">Get solution Delivered in 2 week</div>
+                <div class="text-[12px] sm:text-sm md:text-base font-bold text-[#111457]">Get solution Delivered in 2 week</div>
             </div>
             <div class="flex items-center gap-3 ">
-                <div class="icon h-[16px] sm:h-[20px] md:h-[25px]  w-[16px] sm:w-[20px] md:w-[25px]">
+                <div class="icon h-[14px] sm:h-[20px] md:h-[18px]  w-[14px] sm:w-[14px] md:w-[18px]">
                     <img src="{{asset('public'.mix('/tailwind/img/rightBlue.svg'))}}" alt="thanku" class="img-fluid h-full w-full ">
                 </div>
-                <div class="text-[12px] sm:text-base md:text-lg font-bold text-[#111457]">Dedicated team of food delivery App Developer</div>
+                <div class="text-[12px] sm:text-sm md:text-base font-bold text-[#111457]">Dedicated team of food delivery App Developer</div>
             </div>
         </div>
         <div class="mt-4 flex items-center flex-wrap gap-y-3 gap-3 md:justify-around">
             <div class="flex items-center gap-3">
-                <div class="icon h-[16px] sm:h-[20px] md:h-[25px]  w-[16px] sm:w-[20px] md:w-[25px]">
+                <div class="icon h-[14px] sm:h-[20px] md:h-[18px]  w-[14px] sm:w-[14px] md:w-[18px]">
                     <img src="{{asset('public'.mix('/tailwind/img/rightBlue.svg'))}}" alt="thanku" class="img-fluid h-full w-full ">
                 </div>
-                <div class="text-[12px] sm:text-base md:text-lg font-bold text-[#111457]">Proven Expertise & Cost Effective</div>
+                <div class="text-[12px] sm:text-sm md:text-base font-bold text-[#111457]">Proven Expertise & Cost Effective</div>
             </div>
             <div class="flex items-center gap-3 ">
-                <div class="icon h-[16px] sm:h-[20px] md:h-[25px]  w-[16px] sm:w-[20px] md:w-[25px]">
+                <div class="icon h-[14px] sm:h-[20px] md:h-[18px]  w-[14px] sm:w-[14px] md:w-[18px]">
                     <img src="{{asset('public'.mix('/tailwind/img/rightBlue.svg'))}}" alt="thanku" class="img-fluid h-full w-full ">
                 </div>
-                <div class="text-[12px] sm:text-base md:text-lg font-bold text-[#111457]">100% customizable solutions</div>
+                <div class="text-[12px] sm:text-sm md:text-base font-bold text-[#111457]">100% customizable solutions</div>
             </div>
-        </div>
-        <div class="mt-4 flex items-center flex-wrap gap-y-3 gap-3 md:justify-around">
             <div class="flex items-center gap-3">
-                <div class="icon h-[16px] sm:h-[20px] md:h-[25px]  w-[16px] sm:w-[20px] md:w-[25px]">
+                <div class="icon h-[14px] sm:h-[20px] md:h-[18px]  w-[14px] sm:w-[14px] md:w-[18px]">
                     <img src="{{asset('public'.mix('/tailwind/img/rightBlue.svg'))}}" alt="thanku" class="img-fluid h-full w-full ">
                 </div>
-                <div class="text-[12px] sm:text-base md:text-lg font-bold text-[#111457]">Fully Signed NDA & Code Security</div>
+                <div class="text-[12px] sm:text-sm md:text-base font-bold text-[#111457]">Fully Signed NDA & Code Security</div>
             </div>
         </div>
+        
         <div class="absolute hidden lg:block top-[-2%] left-[-3%] h-[220px] w-[249px]">
             <img src="{{asset('public'.mix('/tailwind/img/pizzaShimg.png'))}}" alt="thanku" class="img-fluid h-full w-full ">
         </div>
@@ -93,38 +102,43 @@
         <div class=" mt-10">
             <a href="https://www.imgglobalinfotech.com/about-us.php" class="inline-flex rounded-br-none justify-center rounded-lg text-sm font-semibold py-2 md:py-3 px-3 md:px-4 bg-slate-900 text-white hover:bg-slate-700"><span class="select-none">Get Your Free Quote <span aria-hidden="true" class="text-slate-400 sm:inline">→</span></span></a>
         </div>
-        <div class="text-sm md:text-base mt-3">
+        {{-- <div class="text-sm md:text-base mt-3">
             All our projects are secured by NDA
-        </div>
-
+        </div> --}}
+    </div>
+</div>
 {{-- hero sectio  end  --}}
 
 {{-- key features section start  --}}
 
 <div class="keyfeature relative overflow-hidden">
-    <div class="mx-auto max-w-7xl px-6 lg:px-8 relative lg:py-20 md:py-16 py-10 mt-40  ">
-        <div class="lg:w-4/5 w-full mx-auto">
-            <div class="font-bold text-[#111457] lg:text-4xl md:text-3xl text-2xl text-center ">Key Features of On-Demand Food Delivery App</div>
-            <div class="text-black font-light text-center sm:text-base text-sm pt-5 leading-6">Escalate your  food delivery & Ordering business to new heights with our fully-featured white-label <span class="font-semibold text-black text-base">on demand restaurant app development solutions.</span>  Our best-in-class certified food app developers develop highly interfeatures for your online food delivery application.</div>
+    <div class="mx-auto max-w-7xl px-6 lg:px-8 relative  md:py-16 py-10   ">
+        <div class="md:pt-3 text-center relative container mx-auto max-w-7xl md:px-6 lg:px-8 z-10">
+            <p class="mt-4 max-md:text-2xl/tight max-xl:text-3xl/tight text-4xl/tight tracking-[-0.04em] text-black">
+                <span class="font-normal">Key Features of On-Demand</span> 
+                <span class="font-bold bg-gradient-to-r from-gray-500 via-sky-600 to-gray-500 bg-clip-text text-transparent"> Food Delivery App</span>
+            </p>
+            <div class="md:text-base text-sm text-black/70 mt-3 mb-lg-1">
+                Escalate your food delivery & Ordering business to new heights with our fully-featured white-label <span class="font-bold opacity-100">on demand restaurant app development solutions.</span>  Our best-in-class certified food app developers develop highly interactive features for your online food delivery application.
+            </div>
         </div>
     </div>
 
-
-    <div class="bg-gray-100 w-full py-3 relative">
+    <div class="bg-gray-100 w-full  relative">
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
             <div class="w-full">
                 <ul class="flex items-center sm:justify-center justify-start gap-9 flex-nowrap w-full whitespace-nowrap overflow-x-auto">
                     <li>
-                        <a href="#customerApp" class="text-decoration-none py-1 sm:text-base text-sm flex items-center font-semibold text-nowrap text-[#111457] relative after:absolute  after:left-0 after:bottom-[-12px] after:w-full after:h-1 after:bg-[#111457] ">Customer App</a>
+                        <a href="#customerApp" class="text-decoration-none pb-4 pt-4 sm:text-base text-sm flex items-center font-semibold text-nowrap text-[#111457] relative after:absolute  after:left-0 after:bottom-0 after:w-full after:h-1 after:bg-[#111457] ">Customer App</a>
                     </li>
                     <li>
-                        <a href="#driverApp" class="text-decoration-none py-1 sm:text-base text-sm flex items-center font-semibold text-nowrap text-[#111457] relative duration-300 after:absolute  after:left-0 after:bottom-[-12px] after:w-0 after:h-1 after:duration-300 after:bg-[#111457] hover:after:w-full">Driver App</a>
+                        <a href="#driverApp" class="text-decoration-none pb-4 pt-4 sm:text-base text-sm flex items-center font-semibold text-nowrap text-[#111457] relative duration-300 after:absolute  after:left-0 after:bottom-0 after:w-0 after:h-1 after:duration-300 after:bg-[#111457] hover:after:w-full">Driver App</a>
                     </li>
                     <li>
-                        <a href="#restaurantApp" class="text-decoration-none py-1 sm:text-base text-sm flex items-center font-semibold text-nowrap text-[#111457] relative duration-300 after:absolute  after:left-0 after:bottom-[-12px] after:w-0 after:h-1 after:duration-300 after:bg-[#111457] hover:after:w-full">Restaurant App</a>
+                        <a href="#restaurantApp" class="text-decoration-none pb-4 pt-4 sm:text-base text-sm flex items-center font-semibold text-nowrap text-[#111457] relative duration-300 after:absolute  after:left-0 after:bottom-0 after:w-0 after:h-1 after:duration-300 after:bg-[#111457] hover:after:w-full">Restaurant App</a>
                     </li>
                     <li>
-                        <a href="#adminPanel" class="text-decoration-none py-1 sm:text-base text-sm flex items-center font-semibold text-nowrap text-[#111457] relative duration-300 after:absolute  after:left-0 after:bottom-[-12px] after:w-0 after:h-1 after:duration-300 after:bg-[#111457] hover:after:w-full">Admin Panel</a>
+                        <a href="#adminPanel" class="text-decoration-none pb-4 pt-4 sm:text-base text-sm flex items-center font-semibold text-nowrap text-[#111457] relative duration-300 after:absolute  after:left-0 after:bottom-0 after:w-0 after:h-1 after:duration-300 after:bg-[#111457] hover:after:w-full">Admin Panel</a>
                     </li>
                 </ul>
             </div>
