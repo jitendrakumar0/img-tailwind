acb1de6be0cb0f77a1c9beef6e8bf7c09e63c08b
shubham agrawal <145680136+shubhamimg@users.noreply.github.com>
2023-12-27 17:21:09 +0530

    jk

--- public/tailwind/js/lazy-content.js
+++ public/tailwind/js/lazy-content.js
@@ -1,3 +1,18 @@
-(()=>{/*!**************************************!*\
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**************************************!*\
   !*** ./resources/js/lazy-content.js ***!
-  \**************************************/document.addEventListener("DOMContentLoaded",function(){var t=document.querySelectorAll('img[loading="lazy"]');"loading"in HTMLImageElement.prototype?t.forEach(function(e){e.src=e.dataset.src,e.removeAttribute("loading")}):document.body.appendChild(Object.assign(document.createElement("script"),{src:"https://polyfill.io/v3/polyfill.min.js?features=loading"}))})})();
+  \**************************************/
+document.addEventListener('DOMContentLoaded', function () {
+  var lazyImages = document.querySelectorAll('img[loading="lazy"]');
+  if ('loading' in HTMLImageElement.prototype) {
+    lazyImages.forEach(function (img) {
+      img.src = img.dataset.src;
+      img.removeAttribute('loading');
+    });
+  } else document.body.appendChild(Object.assign(document.createElement('script'), {
+    src: 'https://polyfill.io/v3/polyfill.min.js?features=loading'
+  }));
+});
+/******/ })()
+;
