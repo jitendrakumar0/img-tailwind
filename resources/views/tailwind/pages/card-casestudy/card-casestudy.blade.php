72d5e220179954637c7409026c9b96dd0ebd5f96
shubham agrawal <145680136+shubhamimg@users.noreply.github.com>
2024-01-29 18:27:21 +0530

    new cards

--- resources/views/tailwind/pages/card-casestudy/card-casestudy.blade.php
+++ resources/views/tailwind/pages/card-casestudy/card-casestudy.blade.php
@@ -631,6 +631,91 @@
     </div>
 {{-- newCard section Ends --}}
 
+{{-- custom Section start --}}
+    <div class="py-5 lg:py-10 mx-auto max-w-7xl px-3 sm:px-6 lg:px-20">
+        <div class="relative container mx-auto max-w-7xl px-6 lg:px-8 z-10 text-center">
+            <p class="
+                max-md:text-2xl/tight max-xl:text-3xl/tight text-4xl/tight tracking-[-0.04em] text-black text-center
+                first:*:font-normal
+                last:*:font-bold last:*:text-theme4">
+                <span>Custom Software</span> 
+                <span> Development Solutions</span>
+            </p>
+            <div class="md:text-base text-sm text-black/70 mt-3 text-balance">
+                Do you want to develop custom software and search for the best IT software development company? Dev Technosys has got you covered. We have years of expertise assisting organizations in achieving their objectives with unique custom software development solutions. Contact our custom software developers and leave the rest to us.<br/>
+                Since 2010, Dev Technosys has been creating transparent, client-focused, and streamlined custom software solutions. Discuss your idea with us, and let’s work together to convert it into revenue-generating software.
+            </div>
+        </div>
+        <div class="flex flex-wrap mt-5 sm:mt-8 md:mt-10 items-center">
+            <div class="w-full lg:w-[40%]">
+                <div class="px-8 lg:aspect-[1/1] aspect-[4/2]">
+                    <img alt="..." src="{{asset('public'.mix('/tailwind/img/about/customSof.webp'))}}" class="w-full h-full object-contain">
+                </div>
+            </div>
+            <div class="w-full lg:w-[60%]">
+                <div class="container pt-1 md:p-5 sm:p-8 p-5 mx-auto max-w-7xl px-6 lg:px-8 relative z-10
+
+                        *:flex *:w-full *:flex-wrap *:pt-5 *:gap-y-8
+
+                        *:*:w-full *:*:sm:w-1/2 *:*:px-4 *:*:sm:pt-0 *:*:pt-4 *:*:sm:pb-1 *:*:pb-8 hover:*:*:md:bg-white hover:*:*:md:shadow-lg *:*:shadow-gray-50 *:*:duration-200 *:*:rounded-xl
+
+                        *:*:*:md:max-w-xs *:*:*:mx-auto
+
+                        [&_svg]:mb-3 [&_svg]:text-theme4
+
+                        [&_.title]:font-medium [&_.title]:text-xl [&_.title]:text-gray-900
+
+                        [&_.tagline]:text-base [&_.tagline]:mt-2 [&_.tagline]:text-gray-600
+                            ">
+                    <ul>
+                        <li>
+                            <div>
+                                <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M25 9.0712C26.527 7.34116 28.7611 6.25 31.25 6.25C35.8524 6.25 39.5833 9.98096 39.5833 14.5833C39.5833 19.1857 35.8524 22.9167 31.25 22.9167C28.7611 22.9167 26.527 21.8255 25 20.0955M31.25 43.75H6.25V41.6667C6.25 34.7631 11.8464 29.1667 18.75 29.1667C25.6536 29.1667 31.25 34.7631 31.25 41.6667V43.75ZM31.25 43.75H43.75V41.6667C43.75 34.7631 38.1536 29.1667 31.25 29.1667C28.9732 29.1667 26.8386 29.7754 25 30.8389M27.0833 14.5833C27.0833 19.1857 23.3524 22.9167 18.75 22.9167C14.1476 22.9167 10.4167 19.1857 10.4167 14.5833C10.4167 9.98096 14.1476 6.25 18.75 6.25C23.3524 6.25 27.0833 9.98096 27.0833 14.5833Z" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </svg>
+                                <p class="title">Startups</p>
+                                <p class="tagline">We find it quite exciting to assist startups in making their business bloom using our custom software development expertise.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M8.33301 12.5C8.33301 10.1988 10.1985 8.33334 12.4997 8.33334H16.6663C18.9675 8.33334 20.833 10.1988 20.833 12.5V16.6667C20.833 18.9679 18.9675 20.8333 16.6663 20.8333H12.4997C10.1985 20.8333 8.33301 18.9679 8.33301 16.6667V12.5Z" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"></path>
+                                    <path d="M29.1663 12.5C29.1663 10.1988 31.0318 8.33334 33.333 8.33334H37.4997C39.8009 8.33334 41.6663 10.1988 41.6663 12.5V16.6667C41.6663 18.9679 39.8009 20.8333 37.4997 20.8333H33.333C31.0318 20.8333 29.1663 18.9679 29.1663 16.6667V12.5Z" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"></path>
+                                    <path d="M8.33301 33.3333C8.33301 31.0322 10.1985 29.1667 12.4997 29.1667H16.6663C18.9675 29.1667 20.833 31.0322 20.833 33.3333V37.5C20.833 39.8012 18.9675 41.6667 16.6663 41.6667H12.4997C10.1985 41.6667 8.33301 39.8012 8.33301 37.5V33.3333Z" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"></path>
+                                    <path d="M29.1663 33.3333C29.1663 31.0322 31.0318 29.1667 33.333 29.1667H37.4997C39.8009 29.1667 41.6663 31.0322 41.6663 33.3333V37.5C41.6663 39.8012 39.8009 41.6667 37.4997 41.6667H33.333C31.0318 41.6667 29.1663 39.8012 29.1663 37.5V33.3333Z" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </svg>
+                                <p class="title">SMBs</p>
+                                <p class="tagline">Custom software solutions unlock multiple business opportunities and increase loyalty and user base for mid-scale companies.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M8.33301 10.4167C8.33301 9.26608 9.26575 8.33334 10.4163 8.33334H39.583C40.7336 8.33334 41.6663 9.26608 41.6663 10.4167V14.5833C41.6663 15.7339 40.7336 16.6667 39.583 16.6667H10.4163C9.26575 16.6667 8.33301 15.7339 8.33301 14.5833V10.4167Z" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"></path>
+                                    <path d="M8.33301 27.0833C8.33301 25.9327 9.26575 25 10.4163 25H22.9163C24.0669 25 24.9997 25.9327 24.9997 27.0833V39.5833C24.9997 40.7339 24.0669 41.6667 22.9163 41.6667H10.4163C9.26575 41.6667 8.33301 40.7339 8.33301 39.5833V27.0833Z" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"></path>
+                                    <path d="M33.333 27.0833C33.333 25.9327 34.2657 25 35.4163 25H39.583C40.7336 25 41.6663 25.9327 41.6663 27.0833V39.5833C41.6663 40.7339 40.7336 41.6667 39.583 41.6667H35.4163C34.2657 41.6667 33.333 40.7339 33.333 39.5833V27.0833Z" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </svg>
+                                <p class="title">Enterprises</p>
+                                <p class="tagline">With our custom software design & development services, we help enterprises integrate and modernize their applications. </p>
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+                                <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M25 9.0712C26.527 7.34116 28.7611 6.25 31.25 6.25C35.8524 6.25 39.5833 9.98096 39.5833 14.5833C39.5833 19.1857 35.8524 22.9167 31.25 22.9167C28.7611 22.9167 26.527 21.8255 25 20.0955M31.25 43.75H6.25V41.6667C6.25 34.7631 11.8464 29.1667 18.75 29.1667C25.6536 29.1667 31.25 34.7631 31.25 41.6667V43.75ZM31.25 43.75H43.75V41.6667C43.75 34.7631 38.1536 29.1667 31.25 29.1667C28.9732 29.1667 26.8386 29.7754 25 30.8389M27.0833 14.5833C27.0833 19.1857 23.3524 22.9167 18.75 22.9167C14.1476 22.9167 10.4167 19.1857 10.4167 14.5833C10.4167 9.98096 14.1476 6.25 18.75 6.25C23.3524 6.25 27.0833 9.98096 27.0833 14.5833Z" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </svg>
+                                <p class="title">Industries</p>
+                                <p class="tagline">We are a custom software development company that helps all types of industries with their existing to future software needs.</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+{{-- custom Section Ends --}}
+
 
 @endsection
 
